4de211eab0c5f9eee985f32753a5d794af0d11d9
tadeeb <tadeeb@Thesis-work.myguest.virtualbox.org>
2025-08-03 16:11:14 +0300

    Phase-3 added

--- project-1.cpp
+++ project-1.cpp
@@ -40,11 +40,32 @@ strcpy(this->ptr,other.ptr);
 }
 }
 
+STR(STR &&other)
+{
+this->ptr=other.ptr;
+other.ptr=NULL;
+}
 
+STR & operator=(STR const &other)
+{
+this->ptr=NULL;
+if(other.ptr)
+{
+this->ptr = new char[strlen(other.ptr)+1];
+if(this->ptr)
+{
+strcpy(this->ptr,other.ptr);
+}
+}
+return *this;
+}
 
-
-
-
+STR & operator=(STR &&other)
+{
+this->ptr=other.ptr;
+other.ptr=NULL;
+return *this;
+}
 
 ~STR()
 {
@@ -79,5 +100,19 @@ STR z=a;
 STR y=j;
 cout<<z<<endl;
 cout<<y<<endl;
+STR i(std::move(a));    
+STR h=std::move(j);    
+cout<<"String value for i: "<<i<<endl;           
+cout<<"String value for h: "<<h<<endl;          
+
+cout<<endl<<"Phase-3"<<endl;
+STR f;
+f=i;
+cout<<"String against f is: "<<f<<endl;
+STR e;
+e=std::move(f);
+cout<<"String against e is: "<<e<<endl;
+cout<<"String against f is: "<<f<<endl;
+
 return 0;
 }
