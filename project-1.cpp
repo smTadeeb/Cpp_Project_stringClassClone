cf101559eccd9bed1211e36a0f167a99c19cb2d7
tadeeb <tadeeb@Thesis-work.myguest.virtualbox.org>
2025-08-03 14:38:21 +0300

    Phase-2 added

--- project-1.cpp
+++ project-1.cpp
@@ -5,11 +5,13 @@ using namespace std;
 class STR
 {
 char *ptr;
+
 public:
 STR()
 {
 this->ptr=NULL;
 }
+
 STR(const char *s)
 {
 if(!s)
@@ -19,12 +21,23 @@ return;
 }
 this->ptr=new char[strlen(s)+1];
 if(!this->ptr) cout<<"Memory Full !!!";
-
 if(this->ptr)
 {
 strcpy(this->ptr,s);
 }
+}
 
+STR(const STR &other)
+{
+this->ptr=NULL;
+if(other.ptr)
+{
+this->ptr = new char[strlen(other.ptr)+1];
+}
+if(this->ptr)
+{
+strcpy(this->ptr,other.ptr);
+}
 }
 
 
@@ -43,12 +56,14 @@ friend ostream &operator<<(ostream &, STR &);
 
 ostream &operator<<(ostream &other1, STR &other2)
 {
+if(other2.ptr==NULL) return other1;
 other1<<other2.ptr;
 return other1;
 }
 
 int main()
 {
+cout<<"Phase-1"<<endl;
 STR a("GOOD");
 STR g="Sheikh Tadeeb";
 STR j(NULL);
@@ -58,5 +73,11 @@ cout<<g<<endl;
 cout<<j<<endl;
 cout<<r<<endl;
 
+
+cout<<"Phase-2"<<endl;
+STR z=a;
+STR y=j;
+cout<<z<<endl;
+cout<<y<<endl;
 return 0;
 }
