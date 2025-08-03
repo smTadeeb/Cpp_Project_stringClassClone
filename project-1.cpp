cfc1ec25f6863850dd4e43c381e26544ce8e3e57
tadeeb <tadeeb@Thesis-work.myguest.virtualbox.org>
2025-08-03 23:26:31 +0300

    Version 2, I will make it into a library later

--- project-1.cpp
+++ project-1.cpp
@@ -5,14 +5,13 @@ using namespace std;
 class STR
 {
 char *ptr;
-
 public:
-STR()
+STR() 					//Default constructor
 {
 this->ptr=NULL;
 }
 
-STR(const char *s)
+STR(const char *s)                    //parametrized constructor
 {
 if(!s)
 {
@@ -20,36 +19,38 @@ this->ptr=NULL;
 return;
 }
 this->ptr=new char[strlen(s)+1];
-if(!this->ptr) return;
+if(!this->ptr) cout<<"Memory Full !!!";
+
 if(this->ptr)
 {
 strcpy(this->ptr,s);
 }
 }
 
-STR(const STR &other)
+STR(const STR &other)            //copy constructor
 {
 this->ptr=NULL;
 if(other.ptr)
 {
 this->ptr = new char[strlen(other.ptr)+1];
 }
-if(!this->ptr) return; //Low-memory case
 if(this->ptr)
 {
 strcpy(this->ptr,other.ptr);
 }
 }
 
-STR(STR &&other)
+STR(STR &&other)              //move constructor
 {
 this->ptr=other.ptr;
 other.ptr=NULL;
 }
 
-STR & operator=(STR const &other)
+STR & operator=(STR const &other) 	//copy-assignment operator        STR & operator=(STR &other, STR *this)  f.=(i,&f)
 {
+if(this->ptr) delete [] this->ptr;
 this->ptr=NULL;
+
 if(other.ptr)
 {
 this->ptr = new char[strlen(other.ptr)+1];
@@ -61,15 +62,19 @@ strcpy(this->ptr,other.ptr);
 return *this;
 }
 
-STR & operator=(STR &&other)
+STR & operator=(STR &&other)     //Move-assignment operator
 {
+if(this->ptr) delete [] this->ptr;
+this->ptr=NULL;     //  <-- We should have a habit of assigning the ptr to NULL after releasing the memory.
 this->ptr=other.ptr;
 other.ptr=NULL;
 return *this;
 }
 
-
-STR & operator=(const char *s)
+// By writing const we are promising that the address 
+// you are giving us we wont change the values using 
+// the pointer we are using to access your data.
+STR & operator=(const char *s)   
 {
 if(this->ptr) delete [] this->ptr;
 this->ptr = NULL;
@@ -78,9 +83,64 @@ if(this->ptr) strcpy(this->ptr,s);
 return *this;
 }
 
+//string1.<(string2,&string1);
+int operator<(const STR &other)
+{
+if(this->ptr==NULL && other.ptr==NULL) return 0;
+if(this->ptr == NULL) return 1;
+if(other.ptr == NULL) return 0;  
+if(strcmp(this->ptr,other.ptr) < 0) return 1;
+else return 0;
+}
+
+int operator<=(const STR &other)
+{
+if(this->ptr==NULL && other.ptr==NULL) return 1;
+if(this->ptr == NULL) return 1;
+if(other.ptr == NULL) return 0;  
+if(strcmp(this->ptr,other.ptr) <= 0) return 1;
+else return 0;
+}
+
+int operator>(const STR &other)
+{
+if(this->ptr==NULL && other.ptr==NULL) return 0;
+if(this->ptr == NULL) return 0;
+if(other.ptr == NULL) return 1;  
+if(strcmp(this->ptr,other.ptr) > 0) return 1;
+else return 0;
+}
+
+int operator>=(const STR &other)
+{
+if(this->ptr==NULL && other.ptr==NULL) return 1;
+if(this->ptr == NULL) return 0;
+if(other.ptr == NULL) return 1;  
+if(strcmp(this->ptr,other.ptr) >= 0) return 1;
+else return 0;
+}
+
+int operator==(const STR &other)
+{
+if(this->ptr==NULL && other.ptr==NULL) return 0;
+if(this->ptr == NULL) return 1;
+if(other.ptr == NULL) return 0;  
+if(strcmp(this->ptr,other.ptr) == 0) return 1;
+else return 0;
+}
+
+int operator!=(const STR &other)
+{
+if(this->ptr==NULL && other.ptr==NULL) return 0;
+if(this->ptr == NULL) return 1;
+if(other.ptr == NULL) return 1;  
+if(strcmp(this->ptr,other.ptr) != 0) return 1;
+else return 0;
+}
+
 ~STR()
 {
-if(this->ptr) delete this->ptr;
+if(this->ptr) delete [] this->ptr;
 }
 
 friend ostream &operator<<(ostream &, STR &);
@@ -88,13 +148,12 @@ friend ostream &operator<<(ostream &, STR &);
 
 ostream &operator<<(ostream &other1, STR &other2)
 {
-if(other2.ptr==NULL) return other1;
-other1<<other2.ptr;
+if(other2.ptr==NULL) return other1;   
+other1<<other2.ptr;   
 return other1;
 }
 
 int main()
 {
-
 return 0;
 }
